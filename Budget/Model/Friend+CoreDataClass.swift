025bb200d757576e8245d49716b61865c153aa4f
Samuel Ivarsson <isamuel@student.chalmers.se>
2022-04-20 03:18:33 +0200

    Added friends and notification views

---
+++ Budget/Model/Friend+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Friend+CoreDataClass.swift
+//  Budget
+//
+//  Created by Samuel Ivarsson on 2022-04-19.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Friend)
+public class Friend: NSManagedObject {
+
+}
