7f936ee45eebf603187f0b7c1448f87c18c67183
Samuel Ivarsson <isamuel@student.chalmers.se>
2023-03-28 11:31:32 +0200

    Added Widget extension

---
+++ Widget/WidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetBundle.swift
+//  Widget
+//
+//  Created by Samuel Ivarsson on 2023-03-27.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Widget()
+        WidgetLiveActivity()
+    }
+}
